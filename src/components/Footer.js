8a1e67296211cd9382b2597428c1fac1ac79583a
James Caleca <jacaleca2@gmail.com>
2022-07-01 16:31:49 -0600

    Styling changes for footer. Added icons there.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import Logo from '../assets/Logo/Black/N4-Enterprises-Black.png'
+import FB from '../assets/Icons/facebook-brands.svg'
+import Insta from '../assets/Icons/instagram-brands.svg'
+import Twitter from '../assets/Icons/twitter-brands.svg'
+import YT from '../assets/Icons/youtube-brands.svg'
 
 export default function Footer() {
     return (
         <footer>
             <div className="footer-cont">
-                <ul>
-                    <li>
-                        <div>
-                            <img alt="n4-logo" src={Logo} className="logo-sm"></img>
-                            <ul className="social-links">
-                                <li>FB</li>
-                                <li>INSTA</li>
-                                <li>TWI</li>
-                                <li>YT</li>
-                            </ul>
-                        </div>
+                <ul className='footer-content'>
+                    <li className="footer-social-li">
+                        <img alt="n4-logo" src={Logo} className="logo-sm"></img>
+                        <ul className="social-links">
+                            <li><img alt="fb-icon" src={FB}></img></li>
+                            <li><img alt="insta-icon" src={Insta}></img></li>
+                            <li><img alt="twitter-icon" src={Twitter}></img></li>
+                            <li><img alt="yt-icon" src={YT}></img></li>
+                        </ul>
                     </li>
-                    <li>
-                        <div>
-                            <ul>
-                                <li>HOME</li>
-                                <li>ABOUT</li>
-                                <li>SERVICES</li>
-                                <li>INSIGHTS</li>
-                                <li>CONTACT</li>
-                            </ul>
-                            <form>
-                                <input type="email" placeholder="Enter your email"></input>
-                                <input type="submit" value="Subscribe Me"></input>
-                            </form>
-                        </div>
+                    <li className="footer-email-li">
+                        <ul className="footer-nav">
+                            <li>HOME</li>
+                            <li>ABOUT</li>
+                            <li>SERVICES</li>
+                            <li>INSIGHTS</li>
+                            <li>CONTACT</li>
+                        </ul>
+                        <form>
+                            <input type="email" placeholder="Enter your email"></input>
+                            <input type="submit" value="Subscribe Me"></input>
+                        </form>
                     </li>
                 </ul>
             </div>
